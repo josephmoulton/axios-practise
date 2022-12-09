baad0ff6b246b80849a8f50391d244f8c34fabc3
Joe <josephlewismoulton@gmail.com>
2022-12-09 14:47:30 +0000

    Functionality added and axios used for future referance

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import { wait } from "@testing-library/user-event/dist/utils";
+
+function Posts() {
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchId, setSearchId] = useState(id);
+
+  function onSearch() {
+    fetchPosts(searchId);
+  }
+
+  async function fetchPosts(userId) {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`
+    );
+    setPosts(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input
+            type="number"
+            value={searchId}
+            onChange={(e) => setSearchId(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && onSearch()}
+          />
+          <button onClick={() => onSearch()}>Enter</button>
+        </div>
+      </div>
+      {loading
+        ? new Array(10).fill(10).map((_, index) => (
+            <div className="post" key={index}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
+    </>
+  );
+}
+
+export default Posts;
